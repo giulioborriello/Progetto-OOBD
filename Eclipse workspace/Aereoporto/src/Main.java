982fa7fca16a2e30ef0b2f1b131c99edc995faf0
Dinvarion <fattiifattituoi25@gmail.com>
2020-12-18 17:58:01 +0100

    Update Main.java

--- Eclipse workspace/Aereoporto/src/Main.java
+++ Eclipse workspace/Aereoporto/src/Main.java
@@ -1,5 +1,3 @@
-package Package1;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -9,6 +7,7 @@ import java.sql.Statement;
 public class Main {
 
 	public static void main(String[] args) {
+		
 		//1.CARICO IL DRIVER
 		try {
 			Class.forName("org.postgresql.Driver");
@@ -16,23 +15,19 @@ public class Main {
 		catch(ClassNotFoundException n)
 		{ System.out.println("Classe Non Trovata\n"+n);
 		//2.OTTENGO CONNESSIONE
+		Connection conn = null;
 		try {
-		Connection conn=DriverManager.getConnection("jdbc:postgresql://localhost:5433/First");
-		Statement st=conn.createStatement();
-		ResultSet rs=st.executeQuery("SELECT * FROM ARTISTA");
-		int i=1;
-		while(rs.next()) {
-		System.out.println("Artista numero"+i);
-		System.out.println("Nome:="+rs.getString("nome"));
-		System.out.println("Cognome:"+rs.getString("cognome"));
-		System.out.println("Nome d'arte:"+rs.getString("nome_d_arte"));
-		i++;
-		}
+		conn=DriverManager.getConnection("jdbc:postgresql://localhost:5432/First");
 		} catch (SQLException e) {
 			System.out.println("Eccezione SQL\n"+e);
 		}
 		//3.ESEGUO LO STATEMENT
-		
+		try {
+			Statement st=conn.createStatement();
+		}
+		catch (SQLException e) {
+			System.out.println("Eccezione SQL\n"+e);
+		}
 		//4.GESTISCO I RISULTATI
 		//5.RILASCIO LE RISORSE
 	}
