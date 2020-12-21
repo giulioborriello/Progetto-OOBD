df8663221ba8621e4629f8f9193fb65c13f560f4
giulioborriello <75851725+giulioborriello@users.noreply.github.com>
2020-12-21 16:46:54 +0100

    Commit 21/12/2020

--- Eclipse workspace/Aereoporto/src/Main.java
+++ Eclipse workspace/Aereoporto/src/Main.java
@@ -1,3 +1,5 @@
+
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -8,28 +10,39 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		//1.CARICO IL DRIVER
-		try {
-			Class.forName("org.postgresql.Driver");
-		}
-		catch(ClassNotFoundException n)
-		{ System.out.println("Classe Non Trovata\n"+n);
+
 		//2.OTTENGO CONNESSIONE
-		Connection conn = null;
 		try {
-		conn=DriverManager.getConnection("jdbc:postgresql://localhost:5432/First");
-		} catch (SQLException e) {
-			System.out.println("Eccezione SQL\n"+e);
+			System.out.println("il programma funziona");
+
+		Connection conn=DriverManager.getConnection("jdbc:postgresql://localhost:5432/First");
+		Statement st=conn.createStatement();
+		ResultSet rs=st.executeQuery("SELECT * FROM Tratta");
+		System.out.println("il programma funziona");
+		int i=1;
+		
+		while(rs.next()) {
+		System.out.println("Tratta numero"+i);
+		System.out.println("CodTratta:"+rs.getString("CodTratta"));
+		System.out.println("Nprenotazioni:"+rs.getInt("Nprenotazioni"));
+		System.out.println("Orario di partenza:"+rs.getTime("Orario di partenza"));
+		System.out.println("Data:" + rs.getDate("Data"));
+		System.out.println("Ngate" + rs.getInt("Ngate"));
+		System.out.println("CodIATA:" + rs.getString("CodIATA"));
+		System.out.println("Destinazione:" + rs.getString("Destinazione"));
+		i++;
 		}
-		//3.ESEGUO LO STATEMENT
-		try {
-			Statement st=conn.createStatement();
-		}
+		
+		} 
+		
 		catch (SQLException e) {
 			System.out.println("Eccezione SQL\n"+e);
 		}
+		//3.ESEGUO LO STATEMENT
+		
 		//4.GESTISCO I RISULTATI
 		//5.RILASCIO LE RISORSE
 	}
 	}
-}
+
+
