a3e65cc1b66fd8204d5c1afea250eff1d5843381
Dinvarion <fattiifattituoi25@gmail.com>
2020-12-21 15:54:02 +0100

    Update Main.java

--- Eclipse workspace/Aereoporto/src/Main.java
+++ Eclipse workspace/Aereoporto/src/Main.java
@@ -10,33 +10,39 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-
+		Connection conn = null;
+		
+		try{
+			conn=DriverManager.getConnection("jdbc:postgresql://localhost:5432/Aereoporto");
+		}
+		catch (SQLException e) {
+			System.out.println("Eccezione 1 SQL\n"+e);
+		}
+		
 		//2.OTTENGO CONNESSIONE
 		try {
 			System.out.println("il programma funziona");
 
-		Connection conn=DriverManager.getConnection("jdbc:postgresql://localhost:5432/First");
 		Statement st=conn.createStatement();
 		ResultSet rs=st.executeQuery("SELECT * FROM Tratta");
 		System.out.println("il programma funziona");
 		int i=1;
 		
 		while(rs.next()) {
-		System.out.println("Tratta numero"+i);
-		System.out.println("CodTratta:"+rs.getString("CodTratta"));
-		System.out.println("Nprenotazioni:"+rs.getInt("Nprenotazioni"));
-		System.out.println("Orario di partenza:"+rs.getTime("Orario di partenza"));
-		System.out.println("Data:" + rs.getDate("Data"));
-		System.out.println("Ngate" + rs.getInt("Ngate"));
-		System.out.println("CodIATA:" + rs.getString("CodIATA"));
-		System.out.println("Destinazione:" + rs.getString("Destinazione"));
-		i++;
-		}
-		
+			System.out.println("Tratta numero"+i);
+			System.out.println("CodTratta:"+rs.getString("CodTratta"));
+			System.out.println("Nprenotazioni:"+rs.getInt("Nprenotazioni"));
+			System.out.println("Orario di partenza:"+rs.getTime("Orario di partenza"));
+			System.out.println("Data:" + rs.getDate("Data"));
+			System.out.println("Ngate" + rs.getInt("Ngate"));
+			System.out.println("CodIATA:" + rs.getString("CodIATA"));
+			System.out.println("Destinazione:" + rs.getString("Destinazione"));
+			i++;
+			}
 		} 
 		
 		catch (SQLException e) {
-			System.out.println("Eccezione SQL\n"+e);
+			System.out.println("Eccezione 2 SQL\n"+e);
 		}
 		//3.ESEGUO LO STATEMENT
 		
