dcbbbb69760041e5fdfa862605100e64eb6e069d
Dinvarion <fattiifattituoi25@gmail.com>
2020-12-21 16:39:25 +0100

    Update Main.java

--- Eclipse workspace/Aereoporto/src/Main.java
+++ Eclipse workspace/Aereoporto/src/Main.java
@@ -9,42 +9,30 @@ import java.sql.Statement;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		
 		try{
 			Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Aereoporto", "postgres", "abcd");
-		
-		
-		//2.OTTENGO CONNESSIONE
-		
+			Statement st=conn.createStatement();
+			ResultSet rs=st.executeQuery("SELECT * FROM public.\"Tratta\"");
 			System.out.println("il programma funziona");
-
-		Statement st=conn.createStatement();
-		ResultSet rs=st.executeQuery("SELECT * FROM tratta");
-		System.out.println("il programma funziona");
-		int i=1;
+			int i=1;
 		
-		while(rs.next()) {
-			System.out.println("Tratta numero"+i);
-			System.out.println("CodTratta:"+rs.getString("CodTratta"));
-			System.out.println("Nprenotazioni:"+rs.getInt("Nprenotazioni"));
-			System.out.println("Orario di partenza:"+rs.getTime("Orario di partenza"));
-			System.out.println("Data:" + rs.getDate("Data"));
-			System.out.println("Ngate" + rs.getInt("Ngate"));
-			System.out.println("CodIATA:" + rs.getString("CodIATA"));
-			System.out.println("Destinazione:" + rs.getString("Destinazione"));
-			i++;
+			while(rs.next()) {
+				System.out.println("Tratta numero"+i);
+				System.out.println("CodTratta:"+rs.getString("CodTratta"));
+				System.out.println("Nprenotazioni:"+rs.getInt("Nprenotazioni"));
+				System.out.println("Orario di partenza:"+rs.getTime("Orario di partenza"));
+				System.out.println("Data:" + rs.getDate("Data"));
+				System.out.println("Ngate" + rs.getInt("Ngate"));
+				System.out.println("CodIATA:" + rs.getString("CodIATA"));
+				System.out.println("Destinazione:" + rs.getString("Destinazione"));
+				i++;
 			}
 		} 
 		
 		catch (SQLException e) {
 			System.out.println("Eccezione 2 SQL\n"+e);
 		}
-		//3.ESEGUO LO STATEMENT
-		
-		//4.GESTISCO I RISULTATI
-		//5.RILASCIO LE RISORSE
 	}
-	}
+}
 
 
