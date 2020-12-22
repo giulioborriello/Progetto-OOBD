75bfdd0f30b86c131d14c4dcc6997d00f2af777b
giulioborriello <75851725+giulioborriello@users.noreply.github.com>
2020-12-22 17:51:28 +0100

    Update BigliettoDAO.java

--- Eclipse workspace/Aereoporto/src/DAO/BigliettoDAO.java
+++ Eclipse workspace/Aereoporto/src/DAO/BigliettoDAO.java
@@ -1,4 +1,6 @@
 package DAO;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -6,6 +8,9 @@ import java.sql.Statement;
 public class BigliettoDAO {
 	ResultSet rs;
 	Statement st = null;
+	Connection conn = null;
+	PreparedStatement StGet = null;
+	
 	public void getAllBiglietto() {
 		try {
 			rs=st.executeQuery("SELECT * FROM public.\"Biglietto\"");
@@ -27,11 +32,55 @@ public class BigliettoDAO {
 			}	
 		}
 
-	public void GetBigliettoByCodFiscale() {
-		rs=st.executeQuery("SELECT * FROM public.\"Biglietto\" WHERE ");
-
-		
-		
+	public void GetBigliettoByCodFiscale(String CodFiscale){
+		try {
+			StGet = conn.prepareStatement("SELECT * FROM public.\"Biglietto\" WHERE \"CodFiscale\" = ?");
+			StGet.setString(1, CodFiscale);
+			ResultSet rs=StGet.executeQuery();
+			while(rs.next()) {
+				System.out.println("CodFiscale:"+rs.getString("CodFiscale"));
+				System.out.println("Tipo di biglietto:"+rs.getString("Tipo di biglietto"));
+				System.out.println("CodBiglietto:"+rs.getString("CodBiglietto"));
+				System.out.println("CodTratta:" + rs.getString("CodTratta"));
+				System.out.println("Posto:" + rs.getString("Posto"));
+				}
+		}
+		catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+	}
+	
+	public void GetBigliettoByCodTratta(String CodTratta){
+		try {
+			StGet = conn.prepareStatement("SELECT * FROM public.\"Biglietto\" WHERE \"CodTratta\" = ?");
+			StGet.setString(1, CodTratta);
+			ResultSet rs=StGet.executeQuery();
+			while(rs.next()) {
+				System.out.println("CodFiscale:"+rs.getString("CodFiscale"));
+				System.out.println("Tipo di biglietto:"+rs.getString("Tipo di biglietto"));
+				System.out.println("CodBiglietto:"+rs.getString("CodBiglietto"));
+				System.out.println("CodTratta:" + rs.getString("CodTratta"));
+				System.out.println("Posto:" + rs.getString("Posto"));
+				}
+		}
+		catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+	
+	public void InsertBiglietto(String CodFiscale)	{
 		
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
