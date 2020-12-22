06bb242454caf50842ee94f6059918186299ecfd
giulioborriello <75851725+giulioborriello@users.noreply.github.com>
2020-12-22 17:09:28 +0100

    aggiunte a Tratta

--- Eclipse workspace/Aereoporto/src/Main.java
+++ Eclipse workspace/Aereoporto/src/Main.java
@@ -1,5 +1,5 @@
 
-
+import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -7,12 +7,22 @@ import java.sql.SQLException;
 public class Main {
 
 	public static void main(String[] args) {
+		Connection conn = null;
 		try{
-			Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Aereoporto", "postgres", "abcd");
+			 conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Aereoporto", "postgres", "abcd");
 			}
 		catch (SQLException e) {
 			System.out.println("Eccezione 2 SQL\n"+e);
 		}
+	try {
+		Statement st = conn.createStatement();
+		TrattaDAO TDAO = new TrattaDAO();
+		TDAO.VerificaFormatoCodTratta(st);
+	} 
+	catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
 	}
 }
 
--- Eclipse workspace/Aereoporto/src/TrattaDAO.java
+++ Eclipse workspace/Aereoporto/src/TrattaDAO.java
@@ -4,27 +4,45 @@ import java.sql.Statement;
 
 
 public class TrattaDAO {
-	
-	public void getTratta(Statement st) {
-		try {
-			ResultSet rs=st.executeQuery("SELECT * FROM public.\"Tratta\"");
-			System.out.println("il programma funziona");
-			int i=1;
+	Statement st = null;
 
-			while(rs.next()) {
-				System.out.println("Tratta numero"+i);
-				System.out.println("CodTratta:"+rs.getString("CodTratta"));
-				System.out.println("Nprenotazioni:"+rs.getInt("Nprenotazioni"));
-				System.out.println("Orario di partenza:"+rs.getTime("Orario di partenza"));
-				System.out.println("Data:" + rs.getDate("Data"));
-				System.out.println("Ngate" + rs.getInt("Ngate"));
-				System.out.println("CodIATA:" + rs.getString("CodIATA"));
-				System.out.println("Destinazione:" + rs.getString("Destinazione"));
-				i++;
+	public TrattaDAO(Statement st) {
+			super();
+			this.st = st;
+		}
+	//	public void getAllTratta(st) {
+	//		try {
+	//			ResultSet rs=st.executeQuery("SELECT * FROM public.\"Tratta\"");
+	//			System.out.println("il programma funziona");
+	//			int i=1;
+	//
+	//			while(rs.next()) {
+	//				System.out.println("Tratta numero"+i);
+	//				System.out.println("CodTratta:"+rs.getString("CodTratta"));
+	//				System.out.println("Nprenotazioni:"+rs.getInt("Nprenotazioni"));
+	//				System.out.println("Orario di partenza:"+rs.getTime("Orario di partenza"));
+	//				System.out.println("Data:" + rs.getDate("Data"));
+	//				System.out.println("Ngate" + rs.getInt("Ngate"));
+	//				System.out.println("CodIATA:" + rs.getString("CodIATA"));
+	//				System.out.println("Destinazione:" + rs.getString("Destinazione"));
+	//				i++;
+	//			}
+	//		}
+	//		catch (SQLException e) {
+	//			// TODO Auto-generated catch block
+	//			e.printStackTrace();
+	//		}	
+	//	}
+		
+		public void GetTrattaByCodTratta(st){
+			
+		}
+		public void VerificaFormatoCodTratta( st) {
+			 try {
+				st.executeUpdate("ALTER TABLE \"Tratta\" ADD CONSTRAINT Verifica_Formato_CodTratta CHECK(\"CodTratta\" ~* '^[a-zA-Z] + [a-zA-Z] + [a-zA-Z] + % +$ ')");
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}	
-	}
+		}
 }
