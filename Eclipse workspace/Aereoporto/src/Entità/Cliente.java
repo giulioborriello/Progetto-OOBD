5d6423718a6a55612bf348de11d2b414bdd7405c
benMK-cloud <b.gazzara@studenti.unina.it>
2020-12-23 17:07:23 +0100

    Bah

---
+++ Eclipse workspace/Aereoporto/src/Entità/Cliente.java
@@ -0,0 +1,42 @@
+package Entità;
+
+public class Cliente {
+public Cliente(String codFiscale, String nome, String cognome, String email) {
+		super();
+		CodFiscale = codFiscale;
+		Nome = nome;
+		Cognome = cognome;
+		Email = email;
+	}
+public String getCodFiscale() {
+		return CodFiscale;
+	}
+	public void setCodFiscale(String codFiscale) {
+		CodFiscale = codFiscale;
+	}
+	public String getNome() {
+		return Nome;
+	}
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+	public String getCognome() {
+		return Cognome;
+	}
+	public void setCognome(String cognome) {
+		Cognome = cognome;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+private String CodFiscale;
+private String Nome;
+private String Cognome;
+private String Email;
+	public Cliente() {
+			}
+
+}
