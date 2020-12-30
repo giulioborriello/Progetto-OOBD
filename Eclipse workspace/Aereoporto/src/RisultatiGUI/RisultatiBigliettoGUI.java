405a40f3446318581ae9f79d8aab97d8d600b64f
PaoloGreco-unina <76002411+PaoloGreco-unina@users.noreply.github.com>
2020-12-30 17:15:37 +0100

    Aggiornamento inseimento dati nel jList

--- Eclipse workspace/Aereoporto/src/RisultatiGUI/RisultatiBigliettoGUI.java
+++ Eclipse workspace/Aereoporto/src/RisultatiGUI/RisultatiBigliettoGUI.java
@@ -7,36 +7,51 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import Entità.Biglietto;
+import Entità.Gate;
+import GUI.Controller;
+
+import javax.swing.JList;
+import javax.swing.ListModel;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import java.awt.Font;
+import java.util.List;
+
 public class RisultatiBigliettoGUI extends JFrame {
 
 	private JPanel contentPane;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					RisultatiBigliettoGUI frame = new RisultatiBigliettoGUI();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
-	public RisultatiBigliettoGUI() {
+	Controller controller;
+	
+	public RisultatiBigliettoGUI(List<Biglietto> list, Controller c) {
+		controller = c;
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 653, 543);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		
+		DefaultListModel<String> listModel = new DefaultListModel<String>();
+		
+		for(Biglietto biglietto: list) {
+			String string = biglietto.getCodBiglietto() + " " + biglietto.getCodFiscale() + " " + biglietto.getNome()
+			+ " " + biglietto.getPosto() + " " + biglietto.getTipo_Di_Biglietto() + " " 
+					+ biglietto.getCodTratta();
+			listModel.addElement(string);
+			
+		}
+		
+		
+		
+		JList jlist = new JList(listModel);
+		jlist.setBounds(34, 11, 400, 363);
+		contentPane.add(jlist);
+		
+		JButton btnNewButton_3 = new JButton("Torna indietro ");
+		btnNewButton_3.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnNewButton_3.setBounds(34, 414, 180, 57);
+		contentPane.add(btnNewButton_3);
 	}
-
 }
