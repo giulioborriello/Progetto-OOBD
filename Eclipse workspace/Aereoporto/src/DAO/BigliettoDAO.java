71158be4782f316e5716a8038bc686a26de6d68c
Paolo Greco <>
2021-02-04 13:45:24 +0100

    Ultime correzioni

--- Eclipse workspace/Aereoporto/src/DAO/BigliettoDAO.java
+++ Eclipse workspace/Aereoporto/src/DAO/BigliettoDAO.java
@@ -7,11 +7,8 @@ import Entità.Biglietto;
 public interface BigliettoDAO {
 	
 	public List<Biglietto> getBigliettoByCodFiscale(String CodFiscale);
-
 	public List<Biglietto> getBigliettoByCodTratta(String CodTratta);
-	
 	public String insertBiglietto(String CodFiscale, String TipoDiBiglietto, String CodBiglietto, String CodTratta, String Posto);
-	
 	public String deleteBiglietto(String CodBiglietto);
 	
 }
--- Eclipse workspace/Aereoporto/src/DAO/CodaDiImbarcoDAO.java
+++ Eclipse workspace/Aereoporto/src/DAO/CodaDiImbarcoDAO.java
@@ -9,12 +9,9 @@ public interface CodaDiImbarcoDAO {
 	
 	
 	public CodaDiImbarco getCodaDiImbarcoByCodCoda(String CodCoda);
-	
 	public CodaDiImbarco getCodaDiImbarcoByCodGate(String CodGate);
-	
 	public CodaDiImbarco getCodaDiImbarcoByCodSlot(String CodSlot);
 	public String insertCodaDiImbarco(String CodCoda, String Tipo_di_coda, String Ngate);
-	
 	public String deleteCodaDiImbarco(String CodCoda);
 	
 }
