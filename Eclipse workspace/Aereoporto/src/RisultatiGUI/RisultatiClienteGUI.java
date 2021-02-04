47f4573a18ce331073564a3eef3f10a08c6bd092
Paolo Greco <>
2021-02-04 16:01:09 +0100

    Aggiunta tabella

--- Eclipse workspace/Aereoporto/src/RisultatiGUI/RisultatiClienteGUI.java
+++ Eclipse workspace/Aereoporto/src/RisultatiGUI/RisultatiClienteGUI.java
@@ -3,6 +3,8 @@ package RisultatiGUI;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
 
 import ControllerPackage.Controller;
 import Entità.Cliente;
@@ -12,14 +14,17 @@ import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import java.awt.Font;
 import java.util.List;
+import java.util.Vector;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JTable;
 
 public class RisultatiClienteGUI extends JFrame {
 	
 	private Controller controller;
 	
 	private JPanel contentPane;
+	private JTable table;
 	
 
 	public RisultatiClienteGUI(List<Cliente> list, Controller c) {
@@ -33,21 +38,7 @@ public class RisultatiClienteGUI extends JFrame {
 		setTitle("Risultati Cliente");
 		setResizable(false);
 		
-		DefaultListModel<String> listModel = new DefaultListModel<String>();
-		String titoli = "Codice Fiscale " + "Nome " + "Cognome "+ "Email ";
-		listModel.addElement(titoli);
 		
-		
-		for(Cliente cliente: list) {
-			String string = cliente.getCodFiscale()+ " " + cliente.getNome() + " " + cliente.getCognome() + " " + cliente.getEmail();
-			listModel.addElement(string);
-			
-		}
-		
-		
-		JList jlist = new JList(listModel);
-		jlist.setBounds(10, 11, 400, 363);
-		contentPane.add(jlist);
 		
 		JButton btnNewButton_3 = new JButton("Torna indietro ");
 		btnNewButton_3.addActionListener(new ActionListener() {
@@ -58,6 +49,30 @@ public class RisultatiClienteGUI extends JFrame {
 		btnNewButton_3.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		btnNewButton_3.setBounds(10, 385, 180, 57);
 		contentPane.add(btnNewButton_3);
+		
+		
+		
+		Vector<String> columns = new Vector<String>();
+		columns.add("Codice Fiscale");
+		columns.add("Nome");
+		columns.add("Cognome");
+		columns.add("Email");
+		
+		Vector<Vector<Object>> data = new Vector<Vector<Object>>();
+		
+		for(int i = 0; i < list.size(); i++) {
+			Vector<Object> vector = new Vector<Object>();
+			vector.add(list.get(i).getCodFiscale());
+			vector.add(list.get(i).getNome());
+			vector.add(list.get(i).getCognome());
+			vector.add(list.get(i).getEmail());
+			
+			data.add(vector);
+		}
+		
+		
+		table = new JTable(new DefaultTableModel(data, columns));
+		table.setBounds(327, 229, -217, -136);
+		contentPane.add(table);
 	}
-
 }
